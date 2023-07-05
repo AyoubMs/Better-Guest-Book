5f2a4832d17f892ab68f37e8c4974cbc57b9b3c6
AyoubMS <chahayb563@gmail.com>
2023-07-05 12:27:00 +0200

    add app

---
+++ ConsoleUI/ConsoleUIMethods.cs
@@ -0,0 +1,50 @@
+﻿using GuestBookLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class ConsoleUIMethods
+    {
+        internal static void GetGuestInformation(List<GuestModel> guests)
+        {
+
+            string moreGuestsComing = "";
+
+            do
+            {
+                GuestModel guest = new GuestModel();
+
+                guest.FirstName = GetInfoFromConsole("What is your first name: ");
+
+                guest.FirstName = GetInfoFromConsole("What is your last name: ");
+
+                guest.MessageToHost = GetInfoFromConsole("What message would you like to tell your host: ");
+
+                moreGuestsComing = GetInfoFromConsole("Are more guests coming (yes/no): ");
+
+                guests.Add(guest);
+
+                Console.Clear();
+            } while (moreGuestsComing.ToLower() == "yes");
+        }
+
+        internal static void PrintGuests(List<GuestModel> guests)
+        {
+            foreach (GuestModel guest in guests)
+            {
+                Console.WriteLine(guest.GuestInfo);
+            }
+        }
+
+        internal static string GetInfoFromConsole(string message)
+        {
+            Console.Write(message);
+            string output = Console.ReadLine();
+            return output;
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,16 @@
+﻿// Capture the information about each guest (assumption is at least one guest and unknown maximum)
+// Info to capture: First name, Last name, message to the host
+// Once done, loop through each guest and print their info
+
+
+using ConsoleUI;
+using GuestBookLibrary.Models;
+
+List<GuestModel> guests = new List<GuestModel>();
+
+ConsoleUIMethods.GetGuestInformation(guests);
+
+ConsoleUIMethods.PrintGuests(guests);
+
+
+Console.ReadLine();
